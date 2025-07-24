8c0a4ce95f39e84a444518dacb1cc61e61c7f2b0
afstierman <austin.stierman@gmail.com>
2025-07-23 21:41:29 -0500

    resume added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,8 +28,12 @@ export const AboutSection = () => {
                             Contact Me
                         </a>
 
-                        <a href="#contact" className ="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
-                            Download Resume
+                        <a
+                        href="/AustinStiermanResume.pdf"
+                        download
+                        className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                        >
+                        Download Resume
                         </a>
                     </div>
                 </div>
